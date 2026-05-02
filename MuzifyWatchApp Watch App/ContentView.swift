55be85b4ed4134e63f242d5c5a16168d88cdf9a0
Kaelan Willauer <49886320+Netstat-abefinoprstxy@users.noreply.github.com>
2026-05-02 10:03:52 -0700

    watch stage 1

---
+++ MuzifyWatchApp Watch App/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  MuzifyWatchApp Watch App
+//
+//  Created by Kaelan Willauer on 5/2/26.
+//  Copyright © 2026 Maximilian Bauer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MuzifyWatchApp Watch App/MuzifyWatchAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  MuzifyWatchAppApp.swift
+//  MuzifyWatchApp Watch App
+//
+//  Created by Kaelan Willauer on 5/2/26.
+//  Copyright © 2026 Maximilian Bauer. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct MuzifyWatchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
